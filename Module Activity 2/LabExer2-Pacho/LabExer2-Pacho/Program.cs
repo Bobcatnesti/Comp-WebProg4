ef01f69d5c48d5b56a71f56c27befb67a177064c
Girom Kenji Respicio Pacho <114282007+Bobcatnesti@users.noreply.github.com>
2024-09-08 14:37:52 +0800

    Add files via upload

---
+++ Module Activity 2/LabExer2-Pacho/LabExer2-Pacho/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LabExer2_Pacho {
+    public class Program {
+        public static void Main(string[] args) {
+            do
+            {
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                if (str == "exit")
+                {
+                    Console.WriteLine("Program has stopped");
+                    break;
+                }
+                else
+                {
+                    int x = Convert.ToInt32(str);
+                    Console.Write("y = ");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    int sum = x + y;
+                    Console.WriteLine("Result: {0}", sum);
+                }
+            }
+            while (true);
+        }
+    }
+}
